cc05341178ccb6c44100545160e96c5487ee6888
chloejuneaulefresne <chloe.juneau-lefresne.1@ens.etsmtl.ca>
2020-04-14 08:05:28 -0400

    reverted

--- src/main/java/ca/etsmtl/log240/financej/AccountDialog.java
+++ src/main/java/ca/etsmtl/log240/financej/AccountDialog.java
@@ -17,7 +17,7 @@ import javax.swing.*;
 public class AccountDialog extends JDialog {
 
     private Connection conn = null;
-    private AccountDAO dataModel;
+    private AccountTableModel dataModel;
 
     /** Creates new form Account */
     public AccountDialog(java.awt.Frame parent, boolean modal) {
@@ -28,7 +28,7 @@ public class AccountDialog extends JDialog {
 
     public void SetDBConnection(Connection DBConn) {
         conn = DBConn;
-        dataModel = new AccountDAO(conn);
+        dataModel = new AccountTableModel(conn);
         AccountListTable.setModel(dataModel);
     }
 
---
+++ src/main/java/ca/etsmtl/log240/financej/AccountTableModel.java
@@ -0,0 +1,80 @@
+package ca.etsmtl.log240.financej;
+
+import javax.swing.table.AbstractTableModel;
+import java.sql.Connection;
+
+class AccountTableModel extends AbstractTableModel {
+
+    private String[] columnNames = {"Name", "Description"};
+    private AccountDAO dao = null;
+
+    public AccountTableModel(Connection DBConn) {
+        dao = new AccountDAO(DBConn);
+    }
+
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public int getRowCount() {
+        return dao.rowCount();
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col) {
+        return dao.getValueAt(row, col);
+    }
+
+    public Class getColumnClass(int c) {
+        return dao.getValueAt(0, c).getClass();
+    }
+
+    /*
+     * Don't need to implement this method unless your table's
+     * editable.
+     */
+    public boolean isCellEditable(int row, int col) {
+        //Note that the data/cell address is constant,
+        //no matter where the cell appears onscreen.
+        if (col == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void setValueAt(Object value, int row, int col) {
+        String AccountName;
+        AccountName = (String) dao.getValueAt(row, 0);
+        boolean querySuccessful = dao.update("description", (String)value, "name", AccountName);
+        if(querySuccessful) {
+            fireTableCellUpdated(row, col);
+        }
+    }
+
+    public void DeleteAccount(int row) {
+        String AccountName;
+        AccountName = (String) dao.getValueAt(row, 0);
+        boolean querySuccessful = dao.delete("name", AccountName);
+        if(querySuccessful) {
+            fireTableDataChanged();
+        }
+    }
+
+    public int AddAccount(String Name, String Description) {
+        if (Name.length() < 3 || Description.length() < 3) {
+            return 0;
+        }
+        if (Name.length() > 250 || Description.length()  > 250 ) {
+            return 0;
+        }
+        int querySuccessful = dao.create(Name, Description);
+        if(querySuccessful == 0) {
+            fireTableRowsInserted(getRowCount() + 1, getRowCount() + 1);
+        }
+        return querySuccessful;
+    }
+}
